808d14daebadb37a69bb873b0e6935e47a997029
Tomasz Zajac <tomezpl@gmail.com>
2025-04-26 19:40:20 +0100

    style(Header): added logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,6 @@
 import {FunctionComponent} from "preact";
 import styles from "./style.module.scss";
+import tzLogo from '../../assets/tz_logo.png';
 
 type HeaderProps = {
     className?: string;
@@ -11,6 +12,7 @@ export const HeaderClasses = {
 
 const Header: FunctionComponent<HeaderProps> = ({className, ref}) => {
     return <div ref={ref} className={['tz-brand-gradient', HeaderClasses.Root, styles.root, className ?? ''].join(' ')}>
+        <img src={tzLogo} className={styles.tzLogo} />
     </div>
 }
 
