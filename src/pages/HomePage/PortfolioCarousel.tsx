9dbc0be14d6a14c9b5b9049693acc2ecbb800431
Tomasz Zajac <tomezpl@gmail.com>
2025-05-05 14:56:00 +0100

    perf(HomePage/PortfolioCarousel): replaced some animated webp with mp4 due to Chromium lag

--- src/pages/HomePage/PortfolioCarousel.tsx
+++ src/pages/HomePage/PortfolioCarousel.tsx
@@ -1,5 +1,5 @@
 import {PortfolioEntry} from './types.ts';
-import {useCallback, useMemo, useState} from 'preact/hooks';
+import {useCallback, useEffect, useMemo, useState} from 'preact/hooks';
 import {FunctionComponent, ReactNode} from 'react';
 import styles from './portfolio-carousel.module.scss';
 import {IoIosArrowDropleftCircle, IoIosArrowDroprightCircle} from 'react-icons/io';
@@ -8,6 +8,10 @@ import type {VNode} from 'preact';
 type PortfolioCarouselProps = {
     entries: ReadonlyArray<PortfolioEntry>;
     idBase?: string;
+}
+
+function wrapIndex(newIndex: number, {length}: Pick<ReadonlyArray<unknown>, 'length'>) {
+    return newIndex < 0 ? (length - 1) : (newIndex % length);
 }
 
 export function PortfolioCarousel({entries, idBase}: PortfolioCarouselProps) {
@@ -29,9 +33,7 @@ export function PortfolioCarousel({entries, idBase}: PortfolioCarouselProps) {
     const [currentlyActiveSlide, setSlide] = useState<number | null>(null);
 
     const cycleSlide = useCallback((direction: number) => {
-        let wrappedIndex = (currentlyActiveSlide ?? 0) + direction;
-        wrappedIndex = wrappedIndex < 0 ? (flatSlides.length - 1) : (wrappedIndex % flatSlides.length);
-        setSlide(wrappedIndex);
+        setSlide(wrapIndex((currentlyActiveSlide ?? 0) + direction, flatSlides));
     }, [flatSlides, currentlyActiveSlide]);
 
     // With JS enabled, allow cycling the slides via JS, blocking the HTMLAnchorElement default event handler
@@ -46,8 +48,28 @@ export function PortfolioCarousel({entries, idBase}: PortfolioCarouselProps) {
         event.preventDefault();
     }, [cycleSlide]);
 
+    const [canUseJs, setCanUseJs] = useState(false);
+
+    useEffect(() => {
+        setCanUseJs(typeof window !== 'undefined');
+    }, []);
+
+    const slidesToHide = useMemo(() => {
+        if(!canUseJs || typeof currentlyActiveSlide !== 'number') {
+            return [];
+        }
+
+        return flatSlides.reduce((indices, _slide, index) => {
+            if(![wrapIndex(currentlyActiveSlide - 1, flatSlides), currentlyActiveSlide, wrapIndex(currentlyActiveSlide + 1, flatSlides)].includes(index)) {
+                indices.push(index);
+            }
+
+            return indices;
+        }, new Array<number>());
+    }, [canUseJs, currentlyActiveSlide, flatSlides]);
+
     return <div className={styles.root}>
-        {flatSlides.map(({title, textWidth, subtitle, technologies, text, image}, i, {length: total}) => <div
+        {flatSlides.map(({title, textWidth, subtitle, technologies, text, image}, i, {length: total}) => slidesToHide.includes(i) ? null : <div
             data-iscurrentslide={currentlyActiveSlide === i ? 'true' : 'false'}
             className={styles.slideRoot} {...(getId(i) ? {id: getId(i)!} : {})}
             style={textWidth ? {'--text-width': textWidth} : {}}
@@ -59,7 +81,8 @@ export function PortfolioCarousel({entries, idBase}: PortfolioCarouselProps) {
                 {(technologies?.length ?? 0) > 0 ? <><hr /><div className={styles.technologies}>{technologies?.map((tech, j) => <><div key={`dummydiv tech ${j} ${title} ${i}`}>{tech}<span className={styles.techDummy} >{((tech as VNode).props as {alt?: string}).alt}</span></div></>)}</div></> : null}
             </div>
             <div draggable={false} className={styles.imageContainer} style={{'--image-src': `url(${image})`}}>
-                <img draggable={false} loading={i === 0 ? 'eager' : 'lazy'} className={styles.image} src={image} />
+                {image.endsWith('mp4') ? <video disableremoteplayback disablePictureInPicture playsinline muted loop className={styles.imageBg} preload={'auto'} autoplay><source src={image} /></video> : <img decoding={'async'} fetchPriority={'high'} draggable={false} loading={ 'lazy'} className={styles.imageBg} src={image} />}
+                {image.endsWith('mp4') ? <video disableremoteplayback playsinline muted loop  className={styles.image} preload={'auto'} autoplay><source src={image} /></video> : <img decoding={'async'} fetchPriority={'high'} draggable={false} loading={i === 0 ? 'eager' : 'lazy'} className={styles.image} src={image} />}
                 <a draggable={false} onClick={onPrevSlide} className={styles.prevBtn} href={getHref((i === 0 ? total : i) - 1)}><IoIosArrowDropleftCircle size={'100%'} /></a>
                 <a draggable={false} onClick={onNextSlide} className={styles.nextBtn} href={getHref((i + 1) % total)}><IoIosArrowDroprightCircle size={'100%'} /></a>
             </div>
